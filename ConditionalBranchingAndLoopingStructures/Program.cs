0d6a4a1bfc24e900d7e72484c7c5dd14d44b95ed
Alex Oliveira <alexoliveira7x@gmail.com>
2023-10-07 21:27:43 +0100

    add freecodecamp exercises

---
+++ ConditionalBranchingAndLoopingStructures/Program.cs
@@ -0,0 +1,21 @@
+﻿string animalSpecies = "";
+string animalID = "";
+string animalAge = "";
+string animalPhysicalDescription = "";
+string animalPersonalityDescription = "";
+string animalNickname = "";
+
+// variables that support data entry
+int maxPets = 8;
+string? readResult;
+string menuSelection = "";
+
+// array used to store runtime data, there is no persisted data
+string[,] ourAnimals = new string[maxPets, 6];
+
+/*
+    The total number of elements in this array can be calculated by multiplying the number of rows by the number of columns, which is 8 * 6 = 48.
+    Therefore, there will be a total of 48 elements in the ourAnimals array. Each element can hold a string value.
+*/
+Console.WriteLine(ourAnimals.Length); // 48
+Console.WriteLine(ourAnimals);
---
+++ ConditionalBranchingAndLoopingStructures/readme.md
@@ -0,0 +1,9 @@
+# Project
+
+This project is a hands-on of: Guided Project - Develop Conditional Branching and Looping Structures
+
+# References 
+
+https://learn.microsoft.com/en-us/training/modules/guided-project-develop-conditional-branching-looping/3-exercise-build-data-menu-loops
+
+https://www.freecodecamp.org/learn/foundational-c-sharp-with-microsoft/add-logic-to-c-sharp-console-applications/guided-project-develop-conditional-branching-and-looping-structures-in-c-sharp
